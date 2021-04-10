df73f69031f0670a987dfd1fe791b884058a2aeb
Alex <52848335+mrash28@users.noreply.github.com>
2021-04-10 20:03:28 +0530

    modified

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// add sticky navigation bar
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+})

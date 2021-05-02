ec16f954d1387b7fa07a06166c76f9c8c03e9602
Alex <52848335+mrash28@users.noreply.github.com>
2021-05-02 11:57:34 +0530

    search box completed

---
+++ js/search_suggesions.js
@@ -0,0 +1,19 @@
+let suggestions = [
+    "Basmathi",
+    "Rice",
+    "White rice",
+    "Basmathi rice white",
+    "Ceylon black tea",
+    "Tea",
+    "Black tea",
+    "Milk",
+    "Dairy",
+    "Chilli",
+    "Chilli powder",
+    "Carrot",
+    "Tomato",
+    "Cabbage",
+    "Coconut",
+    "Vegetables",
+    "Fruits"
+];

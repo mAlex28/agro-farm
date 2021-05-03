c756fcf23c8d79de489866dc88c0190c0bfd9209
Alex <52848335+mrash28@users.noreply.github.com>
2021-05-03 11:29:45 +0530

    modified cart

--- js/comment.js
+++ js/comment.js
@@ -1,13 +1,13 @@
 function render(data) {
-    var html = "<div class='media-body'><h4 class='media-heading'>John Doe</h4><p class='comPara'>"+data.body+"</p><ul class='list-unstyled list-inline media-detail pull-left'><li><i class='fa fa-calendar'></i>27/02/2014</li><li><i class='fa fa-thumbs-up'></i>13</li></ul><ul class='list-unstyled list-inline media-detail pull-right'><li class=''><a href=''>Like</a></li> <li class=''><a href=''>Reply</a></li> </ul></div>";
+    var html = "<div class='media-body'><h4 class='media-heading'>"+data.name+"</h4><p class='comPara'>"+data.body+"</p><ul class='list-unstyled list-inline media-detail pull-left'><li><i class='fa fa-calendar'></i>27/02/2014</li><li><i class='fa fa-thumbs-up'></i>13</li></ul><ul class='list-unstyled list-inline media-detail pull-right'><li class=''><a href=''>Like</a></li> <li class=''><a href=''>Reply</a></li> </ul></div>";
     $('.media').append(html);
 }
 
 
 $(document).ready(function() {
-    var comnt = {
-        "body": "This is a comment"
-    }
+    var comnt = [
+        {"name" : "John Smith", "body": "This is a comment"}
+];
 
     for(var i = 0; i < comnt.length; i++) {
         render(comnt[i]);
@@ -15,6 +15,7 @@ $(document).ready(function() {
 
     $('#commentBtn').click(function() {
         var addObj = {
+            "name" : "John Smith",
             "body" : $('#message').val()
         };
 

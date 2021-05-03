8bfb907990ce9bdad953c5f98f85376e35777c0f
Alex <52848335+mrash28@users.noreply.github.com>
2021-05-03 09:22:05 +0530

    modified

---
+++ js/comment.js
@@ -0,0 +1,25 @@
+function render(data) {
+    var html = "<div class='media-body'><h4 class='media-heading'>John Doe</h4><p class='comPara'>"+data.body+"</p><ul class='list-unstyled list-inline media-detail pull-left'><li><i class='fa fa-calendar'></i>27/02/2014</li><li><i class='fa fa-thumbs-up'></i>13</li></ul><ul class='list-unstyled list-inline media-detail pull-right'><li class=''><a href=''>Like</a></li> <li class=''><a href=''>Reply</a></li> </ul></div>";
+    $('.media').append(html);
+}
+
+
+$(document).ready(function() {
+    var comnt = {
+        "body": "This is a comment"
+    }
+
+    for(var i = 0; i < comnt.length; i++) {
+        render(comnt[i]);
+    }
+
+    $('#commentBtn').click(function() {
+        var addObj = {
+            "body" : $('#message').val()
+        };
+
+        console.log(addObj);
+        comnt.push(addObj);
+        render(addObj);
+    });
+});
